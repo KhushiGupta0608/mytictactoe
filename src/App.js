61e7da40f09ee16c27ac227b069b4a71635f7126
Khushi <khushiguptadpr@gmail.com>
2021-06-20 19:11:09 +0530

    added board state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+import Board from './components/Board';
+import './styles/root.scss';
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>TIC TAC TOE!</h1>
-      <p>Enjoy your game!</p>
-    </>
+      <Board />
+    </div>
   );
 };
 
